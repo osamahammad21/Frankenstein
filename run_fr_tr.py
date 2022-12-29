a5b2dd9b9e09ff3bc2e4a33b682f6992041fb6ea
Osama Hammad <osama21@aucegypt.edu>
2022-12-29 17:57:11 +0000

    distribute to k8s
    Signed-off-by: Osama Hammad <osama21@aucegypt.edu>

--- run_fr_tr.py
+++ run_fr_tr.py
@@ -1,16 +1,17 @@
 import os
 import sys
-
-
+from k8swrapper import K8SExecutor
+import time
 numSeeds_fr = 50
 numSeeds_tr = 1
 curDir = os.getcwd()
-# curDir = '/home/sek006/scratch/03_HybridVigor/runs/ispd18_test4'
-
+executor = K8SExecutor()
 f = open(str(curDir)+'/run_single_fr_tr.py','r')
 lines = f.readlines()
 f.close()
 shared_dir = '/home/oreheem/shared'
+jobs = {}
+remaining = 0
 for seed_fr in range(0,numSeeds_fr):
     fo = open(str(shared_dir)+'/runs/run_fr_tr_%s_%s.py'%(seed_fr,numSeeds_tr),'w')
     for line in lines:
@@ -20,6 +21,18 @@ for seed_fr in range(0,numSeeds_fr):
             line = "numSeeds_tr = %s\n"%(numSeeds_tr)
         fo.write(line)
     fo.close()
-    cmd = 'python3 %s/runs/run_fr_tr_%s_%s.py'%(shared_dir,seed_fr,numSeeds_tr)
-    print(cmd)
+    jobId = executor.runJob('%s/runs/run_fr_tr_%s_%s.py'%(shared_dir,seed_fr,numSeeds_tr))
+    jobs[seed_fr] = {"job_id":jobId,'done':False}
+    remaining += 1
+
+while remaining > 0:
+    time.sleep(60)
+    for seed in jobs:
+        if jobs[seed]['done']:
+            continue
+        status = executor.getJobStatus(jobs[seed]['job_id'])
+        if status.succeeded:
+            remaining -= 1
+            jobs[seed]['done'] = True
+            print('seed {} returned'.format(seed))
 
